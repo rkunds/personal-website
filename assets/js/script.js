f65154cf4e3c7eb90be6da49430c5d8b4e88f8b1
rohit kundu <rkundu3@illinois.edu>
2022-06-09 22:10:48 -0400

    added projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,3 @@
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-    delay: 50,
-});
-
-typewriter.typeString("how's it going!")
-    .pauseFor(1500)
-    .deleteAll()
-    .typeString("i love to swim.")
-    .pauseFor(1000)
-    .deleteChars(5)
-    .typeString("drink boba :)")
-    .pauseFor(1000)
-    .deleteChars(13)
-    .typeString("code.")
-    .pauseFor(1000)
-    .deleteChars(1)
-    .typeString(" (sometimes).")
-    .pauseFor(1500)
-    .deleteAll()
-    .typeString("oh, and don't forget to stay hydrated!")
-    .pauseFor(10000)
-    .deleteAll()
-    .typeString("there's nothing to see after this, you can scroll now!")
-    .pauseFor(10000) 
-    .deleteAll()
-    .typeString("ok, that was a lie. there's nothing after this tho :P")    .start()
-//hide and unhide navbar
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
